55b8d9479f6ea3f6ab768a6ec77b51f104550cd4
LongNet <alongenet32@gmail.com>
2023-03-05 17:20:32 +0300

    Creating Project

---
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.firstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var editTextName: EditText
+    private lateinit var textViewName: TextView
+    private lateinit var buttonSave: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        editTextName = findViewById(R.id.editTextName)
+        textViewName = findViewById(R.id.textViewName)
+        buttonSave = findViewById(R.id.buttonSave)
+
+        buttonSave.setOnClickListener {
+            val name = editTextName.text.toString().trim()
+            textViewName.text = "Привет, $name!"
+        }
+    }
+}
